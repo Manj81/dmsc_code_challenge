f2e3645ac80931e7b413fa2f4ad8a46c9009e5fb
Manj81 <manjeerakishore@gmail.com>
2022-06-27 23:32:36 -0500

    Update validate_metadata.py

--- validate_metadata.py
+++ validate_metadata.py
@@ -11,11 +11,11 @@ def main(source_file, output_file):
         # Copy fieldnames from reader for writer
         fieldnames = reader.fieldnames + ['missing_metadata','metadata_discrepancy']
         # Dictionary for Audio Configuration Code Mapping
-        audio_code_map = {'Standard Stereo': '20',
-                '5.1 (Discrete)': '51',
-                '5.0 (Discrete)': '50',
-                'Lt-Rt (Dolby Surround)': 'DS',
-                'Atmos': 'ATM'}
+        audio_code_map = {'20': 'Standard Stereo',
+                '51': '5.1 (Discrete)',
+                '50': '5.0 (Discrete)',
+                'DS': 'Lt-Rt (Dolby Surround)',
+                'ATM': 'Atmos'}
         # List for included assets that match asset_type_or_class
         included_assets = ['archive',
                 'Audio Stem',
@@ -35,28 +35,24 @@ def main(source_file, output_file):
 
         # Write the fieldnames
         writer.writeheader()
-
         for row in reader:
-            if ('Trailer' in row['folder_names'] or \
-                    row['asset_type_or_class'] not in included_assets):
-                continue
-            output_row = row.copy()
+            if 'Trailer'not in row['folder_names'] \
+                    and row['asset_type_or_class'] in included_assets:
+                output_row = row.copy()
             # Do Something
             # Updating missing metadata
-            output_row['missing_metadata'] = ''
-            output_row['metadata_discrepancy'] = ''
-            for column in metadata_columns:
-                if output_row[column] == '':
-                    output_row['missing_metadata'] += f' {column}'
-            output_row['missing_metadata'] =  output_row['missing_metadata'].lstrip()
-            if (row['custom_metadata.format_details.audio_configuration'] == '' \
-                            or audio_code_map[ \
-                                    row['custom_metadata.format_details.audio_configuration']] != \
-                                    row['name'].split('_')[4]):
-                output_row['metadata_discrepancy'] = 'True'
-            else:
-                output_row['metadata_discrepancy'] = 'False'
-            writer.writerow(output_row)
+                output_row['missing_metadata'] = ''
+                for column in metadata_columns:
+                    if output_row[column] == '':
+                        output_row['missing_metadata'] += f' {column}'
+                output_row['missing_metadata'] =  output_row['missing_metadata'].lstrip()
+                if (row['custom_metadata.format_details.audio_configuration'] == '' \
+                            or row['custom_metadata.format_details.audio_configuration'] != \
+                                    audio_code_map.get(row['name'].split('_')[4], ' ')):
+                    output_row['metadata_discrepancy'] = 'True'
+                else:
+                    output_row['metadata_discrepancy'] = 'False'
+                writer.writerow(output_row)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
